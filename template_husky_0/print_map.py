9ac0536ae5c14ebe25b1dfeb8fc8158ef0492069
matteopriorelli <matteo.priorelli@gmail.com>
2019-09-06 18:22:35 +0200

    added map transfer function

---
+++ template_husky_0/print_map.py
@@ -0,0 +1,29 @@
+import hbp_nrp_cle.tf_framework as nrp
+from hbp_nrp_cle.robotsim.RobotInterface import Topic
+import geometry_msgs.msg
+
+
+@nrp.MapRobotSubscriber('position', Topic('/gazebo/model_states', gazebo_msgs.msg.ModelStates))
+@nrp.MapRobotPublisher('minimap', Topic('/minimap', sensor_msgs.msg.Image))
+@nrp.MapRobotPublisher('scores', Topic('/scores', sensor_msgs.msg.Image))
+@nrp.MapRobotPublisher('loss', Topic('/loss', sensor_msgs.msg.Image))
+def print_map(t, position, minimap, scores, loss):
+    import cv2
+    import rospy
+
+    # display plots
+    i = rospy.get_param('i')
+    cv_img = cv2.imread('/home/efesto/Matteo_WorkSpace/project/plots/map_%d.png' % i)
+    if cv_img is not None:
+        msg_frame = CvBridge().cv2_to_imgmsg(cv_img, 'rgb8')
+        minimap.send_message(msg_frame)
+
+    cv_img = cv2.imread('/home/efesto/Matteo_WorkSpace/project/plots/scores_%d.png' % (i - 1))
+    if cv_img is not None:
+        msg_frame = CvBridge().cv2_to_imgmsg(cv_img, 'rgb8')
+        scores.send_message(msg_frame)
+
+    cv_img = cv2.imread('/home/efesto/Matteo_WorkSpace/project/plots/loss_%d.png' % i)
+    if cv_img is not None:
+        msg_frame = CvBridge().cv2_to_imgmsg(cv_img, 'rgb8')
+        loss.send_message(msg_frame)
